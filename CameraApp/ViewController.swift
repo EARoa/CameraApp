9b32484871f070a8393d82fb59f6f9de08bd6d79
Efrain <jesusayllonroa@gmail.com>
2016-07-13 17:51:35 -0500

    Final

--- CameraApp/ViewController.swift
+++ CameraApp/ViewController.swift
@@ -35,19 +35,14 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate, UINaviga
             imagePickerViewController.delegate = self
             
             let pickFromLibraryAction = UIAlertAction(title: "Pick from Library", style:.Default) { (alert:UIAlertAction) in
-                print("You selected to run from Library")
                 imagePickerViewController.sourceType = .PhotoLibrary
                 self.presentViewController(imagePickerViewController, animated: true, completion: nil)
             }
             
             let takeAPictureAction = UIAlertAction(title: "Take a Picture", style:.Default) { (alert:UIAlertAction) in
-                
                 print("SMILE :D")
-                
                 imagePickerViewController.sourceType = .Camera
                 self.presentViewController(imagePickerViewController, animated: true, completion: nil)
-                
-                
             }
             
             let cancelAction = UIAlertAction(title: "Cancel", style:.Cancel) { (alert:UIAlertAction) in
@@ -70,31 +65,15 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate, UINaviga
             picker.dismissViewControllerAnimated(true, completion:nil)
             
         }
-        
-    
-    
-    
     
     @IBAction func saveImageButton(){
-        
-
         let originalImage = self.photoImageView.image
-        
         UIImageWriteToSavedPhotosAlbum(originalImage!, nil, nil, nil)
         
-        print("O hai")
-        
-        
-        
-        let alert: UIAlertController = UIAlertController(title: "COOL!", message: "Your image has been saved 🎉", preferredStyle: .Alert)
-        alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: {(action: UIAlertAction) -> Void in
+        let alert: UIAlertController = UIAlertController(title: "Hey!", message: "Your image has been saved 🎉", preferredStyle: .Alert)
+        alert.addAction(UIAlertAction(title: "COOL", style: .Default, handler: {(action: UIAlertAction) -> Void in
         }))
         self.presentViewController(alert, animated: true, completion: { _ in })
-
-        
-        
-        
-        
     }
     
         
